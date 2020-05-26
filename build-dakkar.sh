2db175988ede0086f86eb57ac19579f5d099dfb6
niaboc79 <niaboc79@msn.com>
2020-05-26 14:57:10 +0200

    Revert "Update build-dakkar.sh"
    This reverts commit 6c638acc828b742de129f263565462d601d1216b.

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -133,7 +133,7 @@ function clone_or_checkout() {
             git checkout origin/"$localManifestBranch"
         )
     else
-        git clone https://github.com/niaboc79/"$repo" "$dir" -b "$localManifestBranch"
+        git clone https://github.com/phhusson/"$repo" "$dir" -b "$localManifestBranch"
     fi
 }
 
